d6b943f518187f87f29a9106472a4d9376d18fa2
Mr.X <81518780+AlterEgo00001@users.noreply.github.com>
2025-07-08 15:53:28 +0300

    Update chladni.js

--- chladni.js
+++ chladni.js
@@ -5,6 +5,7 @@ import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 
+
 const LANG_PACK = {
   ru: {
     lang_toggle: "Switch to English",
